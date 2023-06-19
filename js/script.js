2a484ce665cf0986b6fe0df25d552832861b765c
Gopinath <gopinath.senthamarai@anywhere.co>
2023-06-19 18:32:45 +0530

    javascript implement

--- js/script.js
+++ js/script.js
@@ -1,10 +1,33 @@
 const menu = document.querySelector(".menu");
+// Menu Toggle Logic
 menu.addEventListener('click', myFunction)
 function myFunction() {
-    var x = document.getElementById("menus");
-    if (x.style.display === "block") {
-        x.style.display = "none";
+    var menu = document.getElementById("menus");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
     } else {
-        x.style.display = "block";
+        menu.style.display = "block";
+    }
+}
+
+// Header fix on top 
+window.onscroll = function () { headerFix() }
+const header = document.querySelector('.header')
+const sticky = header.offsetTop;
+// const headerText = document.getElementsByClassName('.hover')
+function headerFix() {
+    if (window.pageYOffset > innerHeight) {
+        header.style.opacity = 0.5;
+        // header.style.position = "fixed";
+        // headerText.style.color = "red";
+        console.log('trigger')
+        console.log(window.pageYOffset);
+        console.log(window.innerHeight);
+
+    }
+    else {
+        // header.style.position = "static";
+        header.style.opacity = '1';
+        // headerText.style.color = "black"
     }
 }
