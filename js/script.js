8803dbf714f662a59cf07d243ec0c7ad047de0b3
Gopinath <gopinath.senthamarai@anywhere.co>
2023-06-19 12:31:58 +0530

    finish

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-
+const menu = document.querySelector(".menu");
+menu.addEventListener('click', myFunction)
 function myFunction() {
     var x = document.getElementById("menus");
     if (x.style.display === "block") {
