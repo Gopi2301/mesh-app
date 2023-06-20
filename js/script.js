b5c1188e9bdac5b4d8b91ff6f7bd382109eb300a
Gopinath <gopinath.senthamarai@anywhere.co>
2023-06-20 11:59:49 +0530

    javascript ux

--- js/script.js
+++ js/script.js
@@ -1,33 +1,42 @@
 const menu = document.querySelector(".menu");
 // Menu Toggle Logic
 menu.addEventListener('click', myFunction)
+var menuClose = true;
 function myFunction() {
     var menu = document.getElementById("menus");
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "block";
+    menuClose = !menuClose;
+    if (menuClose) {
+        menu.classList.add('menu-open')
     }
+    else {
+        menu.classList.remove('menu-open')
+    }
+    console.log(menuClose);
+
 }
 
 // Header fix on top 
 window.onscroll = function () { headerFix() }
+
 const header = document.querySelector('.header')
-const sticky = header.offsetTop;
-// const headerText = document.getElementsByClassName('.hover')
+
+const headerText = document.querySelectorAll('.hover')
 function headerFix() {
-    if (window.pageYOffset > innerHeight) {
-        header.style.opacity = 0.5;
+    console.log(scrollY);
+    if (scrollY > innerHeight) {
+        header.style.opacity = 0.9;
         // header.style.position = "fixed";
-        // headerText.style.color = "red";
-        console.log('trigger')
-        console.log(window.pageYOffset);
-        console.log(window.innerHeight);
+        header.classList.add('navbar-onchange');
+        // console.log('trigger')
+        // console.log(window.pageYOffset);
+        // console.log(window.innerHeight);
+
 
     }
     else {
+        header.classList.remove('navbar-onchange')
         // header.style.position = "static";
-        header.style.opacity = '1';
-        // headerText.style.color = "black"
+        // header.style.opacity = '1';
+
     }
 }
